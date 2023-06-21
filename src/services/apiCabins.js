ab54e18a4bbb334f486f243fa6fd427ae065d487
NecipAkgz <nexco@hotmail.com>
2023-06-22 01:22:09 +0300

    feat(CreateCabinForm.jsx): add image upload functionality to create cabin form feat(apiCabins.js): upload cabin image to Supabase storage and update cabin record with image URL feat(supabase.js): export supabaseUrl variable for use in other modules feat(FileInput.jsx): set input type to file in FileInput component

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,4 +1,4 @@
-import supabase from './supabase'
+import supabase, { supabaseUrl } from './supabase'
 
 export const getCabins = async () => {
   let { data, error } = await supabase.from('cabins').select('*')
@@ -12,11 +12,30 @@ export const getCabins = async () => {
 }
 
 export const createCabin = async (newCabin) => {
-  const { data, error } = await supabase.from('cabins').insert([newCabin])
+  const imageName = `${Math.random()}-${newCabin.image.name}`.replace('/', '')
+  const imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`
+
+  // 1. Create cabin
+  const { data, error } = await supabase
+    .from('cabins')
+    .insert([{ ...newCabin, image: imagePath }])
 
   if (error) {
     console.log(error)
     throw new Error('Something went wrong ')
+  }
+
+  // 2. Upload Image
+  const { error: storageError } = await supabase.storage
+    .from('cabin-images')
+    .upload(imageName, newCabin.image)
+
+  // 3. Delete cabin if there was a error uploading image
+  if (storageError) {
+    await supabase.from('cabins').delete().q('id', data.id)
+    throw new Error(
+      'Cabin image could not be uploaded and the cabin as not created'
+    )
   }
 
   return data
--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://wicddgjjvfivizlsekyl.supabase.co'
+export const supabaseUrl = 'https://wicddgjjvfivizlsekyl.supabase.co'
 // Row level security enabled
 const supabaseKey =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndpY2RkZ2pqdmZpdml6bHNla3lsIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODcyNzUzNDQsImV4cCI6MjAwMjg1MTM0NH0.Awld5qtf9Tu_IZaKDrHgt3YzB-x_U2KXhaQKyBKJtFM'
