a6db0af2c44f815548e7425feddc593464ca02ee
NecipAkgz <nexco@hotmail.com>
2023-06-21 04:51:32 +0300

    feat(package.json): add @tanstack/react-query, @tanstack/react-query-devtools, and date-fns dependencies feat(App.jsx): add QueryClientProvider and ReactQueryDevtools components to wrap the app and configure QueryClient feat(cabins): add deleteCabin function to apiCabins.js to delete a cabin from the database feat(cabins): add useMutation and useQuery hooks from react-query to CabinRow and CabinTable components to handle cabin deletion and data fetching refactor(cabins): move CabinRow and CabinTable components to features/cabins directory refactor(Cabins.jsx): import CabinTable component from features/cabins directory feat(helpers.js): add formatCurrency function to format numerical values as currency

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import Account from './pages/Account'
 import Bookings from './pages/Bookings'
@@ -9,9 +11,18 @@ import Settings from './pages/Settings'
 import GlobalStyles from './styles/GlobalStyles'
 import AppLayout from './ui/AppLayout'
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+    },
+  },
+})
+
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
@@ -28,7 +39,7 @@ function App() {
           <Route path='*' element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </QueryClientProvider>
   )
 }
 export default App
--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -4,8 +4,19 @@ export const getCabins = async () => {
   let { data, error } = await supabase.from('cabins').select('*')
 
   if (error) {
-    console.log(error.message)
+    console.log(error)
     throw new Error('Cabins could not be loaded')
+  }
+
+  return data
+}
+
+export const deleteCabin = async (id) => {
+  const { data, error } = await supabase.from('cabins').delete().eq('id', id)
+
+  if (error) {
+    console.log(error)
+    throw new Error('Cabins not found')
   }
 
   return data
