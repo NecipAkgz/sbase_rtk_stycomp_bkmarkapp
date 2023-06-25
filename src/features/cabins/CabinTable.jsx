6ccb8b2a116fb899c72f307dd6e1aab4b790c7a4
NecipAkgz <nexco@hotmail.com>
2023-06-26 02:43:10 +0300

    feat(ui/Menus.jsx): add Menus component to create a dropdown menu with a toggle button and a list of buttons feat(features/cabins/CabinRow.jsx): replace the buttons for edit, delete and duplicate with a Menus component to improve the user experience and add a new button for duplicate feat(features/cabins/CabinTable.jsx): wrap the CabinTable component with the Menus component to be able to use the Menus component in the CabinRow component

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import Menus from '../../ui/Menus'
 import Spinner from '../../ui/Spinner'
 import Table from '../../ui/Table'
 import CabinRow from './CabinRow'
@@ -10,21 +11,23 @@ function CabinTable() {
   if (isLoading) return <Spinner />
 
   return (
-    <Table columns='0.6fr 1.8fr 2.2fr 1fr 1fr 1fr'>
-      <Table.Header role='row'>
-        <div></div>
-        <div>Cabin</div>
-        <div>Capacity</div>
-        <div>Price</div>
-        <div>Discount</div>
-        <div></div>
-      </Table.Header>
+    <Menus>
+      <Table columns='0.6fr 1.8fr 2.2fr 1fr 1fr 1fr'>
+        <Table.Header role='row'>
+          <div></div>
+          <div>Cabin</div>
+          <div>Capacity</div>
+          <div>Price</div>
+          <div>Discount</div>
+          <div></div>
+        </Table.Header>
 
-      <Table.Body
-        data={cabins}
-        render={(cabin) => <CabinRow key={cabin.id} cabin={cabin} />}
-      />
-    </Table>
+        <Table.Body
+          data={cabins}
+          render={(cabin) => <CabinRow key={cabin.id} cabin={cabin} />}
+        />
+      </Table>
+    </Menus>
   )
 }
 export default CabinTable
